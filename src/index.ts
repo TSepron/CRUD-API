f6927013a75215d9f7dd06a3a6c638ed043eaed8
TSepron <proffizitdollara@gmail.com>
2022-06-18 19:26:24 +0300

    Implement PUT api/users/{userId}

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import { STATUS_CODE } from './constants/status-codes.js'
 import { parseUrlPathname } from './utils/parseUrlPathname.js'
 import { User } from './orm-database/database.js'
+import { isUuid } from './utils/isUuid.js';
 
 const { PORT } = process.env
 
@@ -14,24 +15,46 @@ createServer(async (req, res) => {
   const reqUrl = new URL(url, `http://${host}`)
 
   const urlPaths = parseUrlPathname(reqUrl)
-  
+
 
   const handleGet = async () => {
+    // GET /api/users
     if (urlPaths.length === 2) {
       const users = await User.all()
 
       res.end(JSON.stringify(users))
     }
 
+    // GET /api/users/${userId}
     if (urlPaths.length === 3) {
-      // const userId = urlPaths[2]
+      const userId = urlPaths[2]
 
-      // const user = await User.find(userId)
+      if (!isUuid(userId)) {
+        res.statusCode = STATUS_CODE.BAD_REQUEST
+        res.end(JSON.stringify({
+          message: 'userId is invalid'
+        }))
+      }
+
+      const user = await User.find(userId)
+
+      if (user == null) {
+        res.statusCode = STATUS_CODE.NOT_FOUND
+        res.end(JSON.stringify({
+          message: `record with id === ${userId} doesn't exist`
+        }))
+      }
+
+      if (res.writableEnded)
+        return 
+
+      res.end(JSON.stringify(user))
     }
   }
 
 
   const handlePost = async (body: string) => {
+    // POST api/users
     if (urlPaths.length === 2) {
       try {
         const user = JSON.parse(body)
@@ -40,15 +63,60 @@ createServer(async (req, res) => {
         res.statusCode = STATUS_CODE.CREATED
         res.end(JSON.stringify(newUser))
       } catch (err) {
-        res.statusCode = STATUS_CODE.BAD_REQUEST
-        res.end(JSON.stringify({
-          message: 'request body does not contain required fields'
-        }))
+        if (err instanceof Error) {
+          if (err.message.endsWith('field required')) {
+            res.statusCode = STATUS_CODE.BAD_REQUEST
+            res.end(JSON.stringify({
+              message: 'request body does not contain required fields'
+            }))
+          } else {
+            throw err
+          }
+        }
       }
     }
   }
 
-  const handlePut = async () => {
+  const handlePut = async (body: string) => {
+    // PUT api/users/{userId}
+    if (urlPaths.length === 3) {
+      const userId = urlPaths[2]
+      const user = JSON.parse(body)
+
+      if (!isUuid(userId)) {
+        res.statusCode = STATUS_CODE.BAD_REQUEST
+        res.end(JSON.stringify({
+          message: 'userId is invalid'
+        }))
+      }
+
+      try {
+        const updatedUser = await User.update(userId, user)
+
+        if (updatedUser == null) {
+          res.statusCode = STATUS_CODE.NOT_FOUND
+          res.end(JSON.stringify({
+            message: `record with id === ${userId} doesn't exist`
+          }))
+        }
+
+        if (res.writableEnded)
+          return
+
+        res.end(JSON.stringify(updatedUser))
+      } catch (err) {
+        if (err instanceof Error) {
+          if (err.message.endsWith('field required')) {
+            res.statusCode = STATUS_CODE.BAD_REQUEST
+            res.end(JSON.stringify({
+              message: 'request body does not contain required fields'
+            }))
+          } else {
+            throw err
+          }
+        }
+      }
+    }
   }
 
 
@@ -60,19 +128,23 @@ createServer(async (req, res) => {
 
   try {
     if (urlPaths[0] === 'api' && urlPaths[1] === 'users') {
+      let body = ''
+
       switch (method) {
         case 'GET':
           await handleGet()
           break
         case 'POST':
-          let body = ''
           for await (const chunk of req) {
             body += chunk.toString()
           }
           await handlePost(body)
           break
         case 'PUT':
-          await handlePut()
+          for await (const chunk of req) {
+            body += chunk.toString()
+          }
+          await handlePut(body)
           break
         case 'DELETE':
           await handleDelete()
--- src/orm-database/database.ts
+++ src/orm-database/database.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { OneUser, PotentialUser } from "../interfaces/database/User.interface"
 import { delay } from './../utils/delay.js';
 
-export class User {
+export class User implements OneUser {
   private static users: OneUser[] = []
 
   id: string
@@ -19,13 +19,13 @@ export class User {
 
   private static validatePotentialUser(user: PotentialUser) {
     if (!user.username)
-      throw new Error('username filed required')
+      throw new Error('username field required')
 
     if (!user.age)
-      throw new Error('age filed required')
+      throw new Error('age field required')
 
     if (!user.hobbies || !Array.isArray(user.hobbies))
-      throw new Error('hobbies filed required')
+      throw new Error('hobbies field required')
   }
 
   static async all() {
@@ -35,14 +35,35 @@ export class User {
     return this.users
   }
 
-  static async create(user: PotentialUser): Promise<OneUser> {
+  static async find(userId: string) {
+    // simulate waiting from real BD
+    await delay(Math.random())
+
+    return this.users.find(user => user.id === userId)
+  }
+
+  static async create(user: PotentialUser) {
     this.validatePotentialUser(user)
-    
+
+    // simulate waiting from real BD
     await delay(Math.random())
 
     const newUser = new this(user)
     this.users.push(newUser)
-    
+
     return newUser
+  }
+
+  static async update(userId: string, updatedUser: PotentialUser) {
+    const foundUser = await this.find(userId)
+
+    if (foundUser == null)
+      return foundUser
+
+    this.validatePotentialUser(updatedUser)
+
+    Object.assign(foundUser, updatedUser)
+
+    return foundUser
   }
 }
